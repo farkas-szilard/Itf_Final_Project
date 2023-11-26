fee65a857dd9779206294f137ad8ce5938100b45
szilard-farkas <farkasszilard13@gmail.com>
2023-11-26 19:50:10 +0200

    organized cunctions, added more scenario outlines

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,25 +1,40 @@
 import time
-
-from selenium.webdriver.common.by import By
+from telnetlib import EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 from base_page import BasePage
 
 
 class LoginPage(BasePage):
-    LOGIN_PAGE_URL = f"{BasePage._BASE_URL}//customer/account/login"
     LOGIN_ICON = (By.PARTIAL_LINK_TEXT, "Sign In")
     EMAIL = (By.ID, "email")
     PASSWORD = (By.ID, "pass")
-    LOGIN_BUTTON = (By.XPATH, '//button[@class="action login primary"]')
+    LOGIN_BUTTON = (By.XPATH, "/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div[2]/form/fieldset/div[4]/div[1]/button/span")
+    DROPDOWN_ARROW = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/span/button")
+    LOGIN_PAGE_URL = f"{BasePage._BASE_URL}//customer/account/login"
+    SIGN_IN = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/a")
+    LOG_OUT_BUTTON = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a")
+    EMAIL_ERROR = (By.ID, "email-error")
+    SIGN_IN_ERROR = (By.XPATH, '//div[@class="message-error error message"]')
+    NO_PASSWORD_ERROR = (By.ID, "pass-error")
+
+    LOGIN_BUTTON = (By.XPATH, "/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div[2]/form/fieldset/div[4]/div[1]/button/span")
     SIGN_IN_ERROR = (By.XPATH, '//div[@class="message-error error message"]')
     EMAIL_ERROR = (By.ID, "email-error")
     WELCOME_BAR = (By.XPATH, "//*[@class='logged-in']")
     NO_PASSWORD_ERROR = (By.ID, "pass-error")
-    dropdown_locator = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/span/button")
+    DROPDOWN_ARROW = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/span/button")
     my_account = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[1]/a")
-    log_out_button = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a")
+    LOG_OUT_BUTTON = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a")
+    SIGN_IN = (By.XPATH, "/html/body/div[2]/header/div[1]/div/ul/li[2]/a")
+    login_fail_error = (By.XPATH, '//*[@id="maincontent"]/div[2]/div[2]/div/div/div')
 
     def navigate_to_login_page(self):
         self.browser.get(BasePage._BASE_URL)
@@ -36,36 +51,60 @@ class LoginPage(BasePage):
     def click_sign_in(self):
         self.click(self.LOGIN_BUTTON)
 
-    def check_for_incorrect_sign_in_error(self):
-        assert self.is_displayed(self.SIGN_IN_ERROR)
+    def click_my_account(self):
+        try:
+            WebDriverWait(self.browser, 2).until(
+                EC.visibility_of_element_located(self.DROPDOWN_ARROW)
+            )
+            # If the element is found within 2 seconds, proceed with the click
+            self.click(self.DROPDOWN_ARROW)
+            time.sleep(3)
+            self.click(self.my_account)
+            time.sleep(2)
+            # Rest of your code here
+        except TimeoutException:
+            # If the element is not found within 2 seconds, enter the else block
+            raise AssertionError("Login credentials are INVALID")
+        # if self.is_displayed(self.DROPDOWN_ARROW):
+        #     self.click(self.DROPDOWN_ARROW)
+        #     time.sleep(2)
+        #     self.click(self.my_account)
+        # else:
+        #     self.click(self.SIGN_IN)
+        #     time.sleep(2)
+        #     self.insert(self.EMAIL, text="email")
+        #     raise AssertionError("Login credentials are INVALID")
+
+    def check_url(self):
+        expected_url = "https://magento.softwaretestingboard.com/customer/account/"
+        self.browser.implicitly_wait(10)
+        # self.click(self.DROPDOWN_ARROW)
+        # # time.sleep(2)
+        # self.click(self.my_account)
+        if expected_url == self.browser.current_url:
+            pass
+        else:
+            self.click(self.SIGN_IN)
+            time.sleep(2)
+            self.insert(self.EMAIL, text="email")
+            raise AssertionError("Login credentials are INVALID")
+
+    def log_out(self):
+        self.click(self.DROPDOWN_ARROW)
+        time.sleep(2)
+        self.click(self.LOG_OUT_BUTTON)
+        time.sleep(2)
 
     def check_for_email_error(self):
         assert self.is_displayed(self.EMAIL_ERROR)
 
-    # def check_email_error_text(self):
-    #     expected_error = "This is a required field."
-    #     error_element = self.browser.find_element(By.ID, "email-error")
-    #     actual_error = error_element.text
-    #     assert expected_error == actual_error
 
-    # def check_email_error_text(self):
-    #     expected_error = {email_error}
-    #     actual_error = self.get_text(self.EMAIL_ERROR)
-    #     assert expected_error == actual_error
     def check_email_error_text(self, expected_error):
         actual_error = self.get_text(self.EMAIL_ERROR)
         assert expected_error == actual_error
 
-    # def check_welcome_msg(self):
-    #     WebDriverWait(self.browser, 10).until(
-    #         EC.visibility_of_element_located(self.WELCOME_BAR)
-    #     )
-    #
-    #     actual_message = self.get_text(self.WELCOME_BAR)
-    #     expected_phrase = "Welcome"
-    #
-    #     # print(f"Actual message: '{actual_message}'")
-    #     assert expected_phrase in actual_message
+    def check_for_incorrect_sign_in_error(self):
+        assert self.is_displayed(self.SIGN_IN_ERROR)
 
     def check_for_no_password_error(self):
         assert self.is_displayed(self.NO_PASSWORD_ERROR)
@@ -74,17 +113,21 @@ class LoginPage(BasePage):
         actual_error = self.get_text(self.NO_PASSWORD_ERROR)
         assert expected_error == actual_error
 
-    def click_my_account(self):
-        self.click(self.dropdown_locator)
-        # time.sleep(2)
-        self.click(self.my_account)
-        # time.sleep(2)
 
-    def check_url(self):
-        self.is_url_correct(f"{BasePage._BASE_URL}/customer/account/")
 
-    def log_out(self):
-        self.click(self.dropdown_locator)
-        time.sleep(2)
-        self.click(self.log_out_button)
-        time.sleep(2)
+    # def check_url(self):
+    #     expected_url = "https://magento.softwaretestingboard.com/customer/account/"
+    #     self.browser.implicitly_wait(10)
+    #     if expected_url == self.browser.current_url:
+    #         pass
+    #     else:
+    #         self.click(self.SIGN_IN)
+    #         time.sleep(2)
+    #         # self.insert(self.EMAIL, text="email")
+    #         raise AssertionError("Login credentials are INVALID")
+    #
+    # def log_out(self):
+    #     self.click(self.DROPDOWN_ARROW)
+    #     time.sleep(2)
+    #     self.click(self.LOG_OUT_BUTTON)
+    #     time.sleep(2)
--- steps/login_steps.py
+++ steps/login_steps.py
@@ -1,29 +1,37 @@
-from behave import  *
+from behave import *
 
 
 @given("I am on the login page")
 def step_imp(context):
     context.login_page.navigate_to_login_page()
 
+
 @when('I insert "{email}" email')
 def step_impl(context, email):
     context.login_page.insert_email(email)
 
+
 @when('I insert "{password}" password')
-def step_impl(context,password):
+def step_impl(context, password):
     context.login_page.insert_password(password)
+
 
 @when("I click on the sign in button")
 def step_impl(context):
     context.login_page.click_sign_in()
 
+
 @when("I click the My Account dropdown menu")
 def step_impl(context):
     context.login_page.click_my_account()
 
-@then("Incorrect sign in error is displayed")
+@then("I can login into the application and see the My Account page")
 def step_impl(context):
-    context.login_page.check_for_incorrect_sign_in_error()
+    context.login_page.check_url()
+
+@then("I log out")
+def step_impl(context):
+    context.login_page.log_out()
 
 @then("Email error is displayed")
 def step_impl(context):
@@ -33,22 +41,26 @@ def step_impl(context):
 def step_imp(context, email_error):
     context.login_page.check_email_error_text(email_error)
 
-# @then("I can login into the application and see the sign in bar displaying the welcome message")
-# def step_imp(context):
-#     context.login_page.check_welcome_msg()
+@then("Incorrect sign in error is displayed")
+def step_impl(context):
+    context.login_page.check_for_incorrect_sign_in_error()
+
+# @then('Email error message contains "{email_error}"')
+# def step_imp(context, email_error):
+#     context.login_page.check_email_error_text(email_error)
+
 
 @then("Password error is displayed")
 def step_imp(context):
     context.login_page.check_for_no_password_error()
 
+
 @then('Password error message contains "{password_error}"')
 def step_imp(context, password_error):
     context.login_page.check_password_error_text(password_error)
 
-@then("I can login into the application and see the My Account page")
-def step_impl(context):
-    context.login_page.check_url()
 
-@then("I log out")
-def step_impl(context):
-    context.login_page.log_out()
+
+
+
+
